8b9b33dbd62b63580e9491ab3a723014822ec125
reza <reza@pop-os.localdomain>
2023-04-18 04:16:05 +0700

    fixing build error

--- src/pages/apps/components/LCMenuCards.jsx
+++ src/pages/apps/components/LCMenuCards.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable */
 import Link from "next/link";
 import Image from "next/image";
+
 export default function LCMenusCard({ Judul, Tamnel, LinkEdit, TemplateLink }) {
   return (
     <div className="h-[340px] m-2 px-1 backdrop-blur-3xl py-2 rounded-md flex flex-col justify-center text-center w-[400px] bg-transparent ">
@@ -12,18 +14,18 @@ export default function LCMenusCard({ Judul, Tamnel, LinkEdit, TemplateLink }) {
           className="w-auto  hover:scale-125 hover:transition-all bg-cover h-auto"
         ></Image>
       </div>
-      <Link
+      <a
         className="font-bold hover:text-aksen my-2 overflow-scroll max-h-[55px]"
         href={TemplateLink}
       >
         {Judul}
-      </Link>
-      <Link
+      </a>
+      <a
         href={LinkEdit}
         className="hover:ease-in-out hover:duration-300 my-2 lg:mx-2 rounded-lg py-2 px-12 font-semibold text-secondary align-middle text-center bg-white hover:bg-aksen hover:text-white mx-auto"
       >
         Edit Color
-      </Link>
+      </a>
     </div>
   );
 }
